c7b75ba765b07dffed839a4dfecf8e046d15621f
SarthakVyas31 <161834375+SarthakVyas31@users.noreply.github.com>
2025-05-29 18:25:42 +0530

    area of rectangle code

---
+++ area_of_rectangle.py
@@ -0,0 +1,4 @@
+width=int(input("enter width of rectangle:"))
+length=int(input("enter length of rectangle:"))
+area=length*width
+print(f"area of rectangle with length = {length} and width = {width} is {area}")
